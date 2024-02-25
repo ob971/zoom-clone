f4541d343b29c890e2c8deb7d3bec2ad14861175
obsa <obsukebede2022@gmail.com>
2024-02-25 17:30:17 +0300

    updating

--- server.js
+++ server.js
@@ -2,32 +2,31 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const { v4: uuidv4 } = require('uuid');
-const io =require('socket.io')(server);
+const io = require('socket.io')(server);
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+});
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 
-
+app.get('/peerjs', peerServer);
 app.get('/', (req, res) => {
   res.redirect(`/${uuidv4()}`);
 });
 
 app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room });
-
 });
 
-
-
-
-
-
-
-
-
-
-
-
+io.on('connection', (socket) => {
+  socket.on('join-room', (roomId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+  });
+});
 
 server.listen(5030, () => {
   console.log('Server is running on port 5030');
